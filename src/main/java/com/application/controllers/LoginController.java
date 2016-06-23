6281ee2847da8e781e05910299e2dbab3be6b4a9
jamesrloveday <jamesrloveday@outlook.com>
2016-06-23 15:50:08 +0100

    Moved forward with adding path

--- src/main/java/com/application/controllers/LoginController.java
+++ src/main/java/com/application/controllers/LoginController.java
@@ -4,6 +4,9 @@ import com.application.service.LoginService;
 import com.application.transferObjects.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,22 +30,14 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login")
-    public String getLoginPage() {
+    public String getLoginPage(@ModelAttribute("user") User user) {
         return "login";
     }
-
-    @RequestMapping(value = "loginUser", method = RequestMethod.POST)
-    public String loginUser(@RequestParam("username") String username,
-                            @RequestParam("password") String password) {
-        final User user = new User(username, password);
-        final boolean loggedIn = loginService.loginUser(user);
-            if(loggedIn) return "home";  
-            else return "redirect:login";
-    }
     
-    @RequestMapping(value = "loginUser", method = RequestMethod.GET)
-    public String loginUser() {
-        final User user = new User();
+        @RequestMapping(value = "/loginUser", 
+                        params = {"login"})
+    public String loginUser(@ModelAttribute("user") User user,
+                            Model model) {
         final boolean loggedIn = loginService.loginUser(user);
             if(loggedIn) return "home";  
             else return "redirect:login";
